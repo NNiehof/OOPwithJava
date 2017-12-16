8c8d02f0ca50acc877d75881bc8710bd2d67dcbf
NNiehof <nynkeniehof@gmail.com>
2017-12-16 17:28:15 +0100

    week1-005

---
+++ 2013-OOProgrammingWithJava-PART1/week1-005.SecondsOfTheYear/src/SecondsOfTheYear.java
@@ -0,0 +1,17 @@
+public class SecondsOfTheYear {
+
+    public static void main(String[] args) {   
+        // Calculate number of seconds in a year
+        
+        int daysInYear = 365;
+        int hoursInDay = 24;
+        int minutesInHour = 60;
+        int secondsInMinute = 60;
+        
+        int secondsInYear = daysInYear * hoursInDay * minutesInHour
+                * secondsInMinute;
+
+        System.out.println("There are " + secondsInYear + " seconds in a year");
+    }
+
+}
