b7a1b3102fab910d23223b303a4846f79b82cce9
NNiehof <nynkeniehof@gmail.com>
2017-12-16 22:33:46 +0100

    week2-028

---
+++ 2013-OOProgrammingWithJava-PART1/week2-028.FromHundredToOne/src/FromHundredToOne.java
@@ -0,0 +1,14 @@
+
+public class FromHundredToOne {
+
+    public static void main(String[] args) {
+        // Print integers 100-1
+        
+        int num = 100;
+        
+        while (num >= 1) {
+            System.out.println(num);
+            num --;
+        }
+    }
+}
