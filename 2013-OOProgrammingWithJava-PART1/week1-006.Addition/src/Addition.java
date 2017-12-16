3ba5e2eb20a331aff2f0f232ef9c8c85c8a48226
NNiehof <nynkeniehof@gmail.com>
2017-12-16 17:32:15 +0100

    week1-006

---
+++ 2013-OOProgrammingWithJava-PART1/week1-006.Addition/src/Addition.java
@@ -0,0 +1,15 @@
+public class Addition {
+
+    public static void main(String[] args) {
+        // Print sum of two numbers
+        
+        int a = 1337;
+        int b = 42;
+        int result = a + b;
+
+        String toPrint = a + " + " + b + " = " + result;
+        System.out.println(toPrint);
+
+    }
+
+}
