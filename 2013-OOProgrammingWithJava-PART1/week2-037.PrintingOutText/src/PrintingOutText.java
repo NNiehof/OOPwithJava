b3b0a33349d6e1e0971a1faea99dcd61059e183f
NNiehof <nynkeniehof@gmail.com>
2017-12-16 23:50:45 +0100

    week2-037

---
+++ 2013-OOProgrammingWithJava-PART1/week2-037.PrintingOutText/src/PrintingOutText.java
@@ -0,0 +1,12 @@
+public class PrintingOutText {
+
+    public static void printText() {
+        // A method to print a Finnish saying
+        
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+
+    public static void main(String[] args) {
+        printText();
+    }
+}
