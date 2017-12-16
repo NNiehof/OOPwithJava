278e891c7f257aafc8073934677c2b2c8d046e56
NNiehof <nynkeniehof@gmail.com>
2017-12-16 17:10:15 +0100

    week1-003

---
+++ 2013-OOProgrammingWithJava-PART1/week1-003.Spruce/src/Spruce.java
@@ -0,0 +1,14 @@
+public class Spruce {
+
+    public static void main(String[] args) {
+
+        // Print a spruce tree
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+    }
+
+}
