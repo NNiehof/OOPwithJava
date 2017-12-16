e7bfd1def93408557abaa950bff68a7365876f26
NNiehof <nynkeniehof@gmail.com>
2017-12-16 17:36:25 +0100

    week1-007

---
+++ 2013-OOProgrammingWithJava-PART1/week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,14 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+        // Print quotient of two numbers
+
+        int a = 1337;
+        int b = 42;
+        int result = a * b;
+
+        String outputLine = a + " * " + b + " = " + result;
+        System.out.println(outputLine);
+    }
+
+}
