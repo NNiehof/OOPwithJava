00c33bc2d5e9b5c50288b9ae0c889c9d283f5aa1
NNiehof <nynkeniehof@gmail.com>
2017-12-17 12:19:19 +0100

    week3-045

---
+++ 2013-OOProgrammingWithJava-PART1/week3-045.Greatest/src/Greatest.java
@@ -0,0 +1,15 @@
+
+public class Greatest {
+
+    public static int greatest(int number1, int number2, int number3) {
+        // Return greatest number
+        int max1 = Math.max(number1, number2);
+        int max2 = Math.max(max1, number3);
+        return max2;
+    }
+
+    public static void main(String[] args) {
+        int result = greatest(2, 7, 3);
+        System.out.println("Greatest: " + result);
+    }
+}
