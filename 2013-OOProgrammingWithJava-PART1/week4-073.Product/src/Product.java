8933aa00f89daae61aed27189c34fceb7ba477e4
NNiehof <nynkeniehof@gmail.com>
2017-12-18 04:56:34 +0100

    week4-073

---
+++ 2013-OOProgrammingWithJava-PART1/week4-073.Product/src/Product.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author NNiehof
+ */
+public class Product {
+    // Represents products sold in a webshop.
+    private String name;
+    private double price;
+    private int amount;
+    
+    public Product(String nameAtStart, double priceAtStart, int amountAtStart) {
+        this.name = nameAtStart;
+        this.price = priceAtStart;
+        this.amount = amountAtStart;
+    }
+    
+    public void printProduct() {
+        System.out.println(this.name + ", price " + this.price + ", amount "
+            + this.amount);
+    }
+}
