07c0252241d083030488e7900c0accdb2e2f1bac
NNiehof <nynkeniehof@gmail.com>
2018-01-17 16:40:07 +0100

    week10-029

---
+++ mooc-2013-OOProgrammingWithJava-PART2/week10-week10_29.PersonAndTheirHeirs/src/people/Student.java
@@ -0,0 +1,31 @@
+package people;
+
+
+/**
+ * Student.java
+ *
+ * @author NNiehof
+ */
+
+public class Student extends Person {
+
+    private int credits;
+    
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+    
+    public void study() {
+        this.credits++;
+    }
+    
+    public int credits() {
+        return this.credits;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "\n  credits " + this.credits;
+    }
+}
